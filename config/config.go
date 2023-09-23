5efe2503b32bdb5fbcb48709395f101c7debba79
evescn <you@example.com>
2023-09-23 18:14:45 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -4,21 +4,24 @@ import "time"
 
 const (
 	WsAddr     = "0.0.0.0:8082"
-	ListenAddr = "0.0.0.0:9090"
+	ListenAddr = "0.0.0.0:9000"
 	// 1个集群使用为多集群
 	Kubeconfigs = `{
-		"DEV": "/Users/evescn/Documents/GitHub/kubea-demo/config/config",
-		"TST": "/Users/evescn/Documents/GitHub/kubea-demo/config/config"
+		"DEV": "./config/dev-config",
+		"TST": "./config/test-config"
 	}`
+
 	// 查看容器日志时，显示的tail行数 tail -n 5000
 	PodLogTailLine = 2000
 	//数据库配置
 	DbType = "mysql"
-	DbHost = "localhost"
-	DbPort = 3306
+	//DbHost = "mysql"
+	//DbPort = 3306
+	DbHost = "10.0.0.101"
+	DbPort = 24858
 	DbName = "kubea_cicd"
-	DbUser = "root"
-	DbPwd  = "123456"
+	DbUser = "kubea_cicd"
+	DbPwd  = "kubea_cicd12321.."
 	//打印mysql debug sql日志
 	LogMode = false
 	//连接池配置
@@ -30,9 +33,10 @@ const (
 
 	//账号密码
 	AdminUser = "admin"
-	AdminPwd  = "123456"
+	AdminPwd  = "admin12321"
 
 	//cicd
 	//编译记录使用
-	JenkinsUrl = "https://test-jenkins.dayuan1997.com/view/%s/job/%s/"
+	JenkinsUrl   = "https://test-jenkins.dayuan1997.com/view/%s/job/%s/"
+	UserPassword = "admin:11b5ec1e1c83e2647675012d26e1381530"
 )
