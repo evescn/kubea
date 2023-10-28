52646876c8f67d95f92055437743325227dd89dd
evescn <519518995@qq.com>
2023-10-28 17:45:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o app .
 
-docker build -t harbor.dayuan1997.com/devops/kubea:v1.$1 .
-docker push harbor.dayuan1997.com/devops/kubea:v1.$1
+docker build -t docker.io/evescn/kubea:v1.$1 .
+docker push docker.io/evescn/kubea:v1.$1
